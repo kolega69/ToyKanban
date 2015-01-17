25cdbcd496acc619cf5ddbaf29f926266afa4c58
oleg <kolega.ua@gmail.com>
2015-01-17 19:48:17 +0200

    add logout possibility

---
+++ src/main/java/authentication/servlets/Logout.java
@@ -0,0 +1,55 @@
+package authentication.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import board.beans.User;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet(name = "Logout", urlPatterns = { "/logout" })
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private final Logger logger = LogManager.getLogger();
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie: cookies) {
+				if(cookie.getName().equals("JSESSIONID")) {
+					logger.info(cookie.getValue());
+				}
+			}
+		}
+		
+		HttpSession session = request.getSession(false);
+		if(session != null) {
+			User user = (User)session.getAttribute("user");
+			logger.info("User: " + user.getName() + "did logout");
+			session.invalidate();
+		}
+		response.sendRedirect("login.html");
+	}
+
+}
