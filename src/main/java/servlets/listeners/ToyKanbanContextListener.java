5baeec259211aaab47d719ab1b3a0b35a197eb6b
oleg <kolega.ua@gmail.com>
2015-01-18 17:04:20 +0200

    added support for the authorized access to the database

--- src/main/java/servlets/listeners/ToyKanbanContextListener.java
+++ src/main/java/servlets/listeners/ToyKanbanContextListener.java
@@ -1,6 +1,7 @@
 package servlets.listeners;
 
 import java.net.UnknownHostException;
+import java.util.Arrays;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -9,6 +10,8 @@ import javax.servlet.annotation.WebListener;
 
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 @WebListener
 public class ToyKanbanContextListener implements ServletContextListener {
@@ -16,15 +19,30 @@ public class ToyKanbanContextListener implements ServletContextListener {
 	MongoClient moClient;
 	DB db;
 
+	/**
+	 * Initialize mongo database and set it to the servlet context
+	 */
 	public void contextInitialized(ServletContextEvent sce) {
-		ServletContext secont = sce.getServletContext();
-		String dbHost = secont.getInitParameter("host");
-		int dbPort = Integer.parseInt(secont.getInitParameter("port")); 
+		ServletContext seCxt = sce.getServletContext();
+		String dbHost = seCxt.getInitParameter("host");
+		int dbPort = Integer.parseInt(seCxt.getInitParameter("port")); 
 		
 		try {
-			moClient = new MongoClient(dbHost, dbPort);
-			db = moClient.getDB("toyKanban");
-			secont.setAttribute("db", db);
+			String userName = seCxt.getInitParameter("userName");
+			String password = seCxt.getInitParameter("password");
+			String database = seCxt.getInitParameter("database");
+			
+			if (userName == null || password == null) {
+				moClient = new MongoClient(dbHost, dbPort);
+			} else {   
+				// database authentication if needed.
+				ServerAddress address = new ServerAddress(dbHost, dbPort);
+				MongoCredential credential = MongoCredential
+						.createMongoCRCredential(userName, database, password.toCharArray());
+				moClient = new MongoClient(address, Arrays.asList(credential));
+			}
+			db = moClient.getDB(database);
+			seCxt.setAttribute("db", db);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
